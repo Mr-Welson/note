07af839affbcf743f53085c617300278c9b89f69
welson <468905342@qq.com>
2023-05-22 15:35:47 +0800

    添加主题切换功能、背景网格切换功能，优化一页截屏行数，根据字数动态调整行数

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,91 @@
 <template>
-  <div class="header">
-    <input class="address" placeholder="请输入知乎章节地址" v-model="address" />
-    <div class="config_list">
-      <div class="config_item">
-        <div class="label">首页行数：</div>
-        <input type="number" class="value" placeholder="请设置首页行数" v-model="firstPageSize" />
+  <div class="layout" :class="{ dark: themeMode === 'dark', light: themeMode === 'light' }">
+    <div class="header">
+      <input class="address" placeholder="请输入知乎章节地址" v-model="address" />
+      <div class="config_list">
+        <div class="config_item">
+          <div class="label">首页行数：</div>
+          <input type="number" class="value" placeholder="请设置首页行数" v-model="firstPageSize" />
+        </div>
+        <div class="config_item">
+          <div class="label">最大行数：</div>
+          <input type="number" class="value" placeholder="请设置一页最大行数" v-model="pageSize" />
+        </div>
+        <div class="config_item">
+          <div class="label">最大页码：</div>
+          <input type="number" class="value" placeholder="请设置截图最大页码" v-model="maxPage" />
+        </div>
+        <div class="config_item">
+          <div class="label">网格背景：</div>
+          <div class="value radio_wrapper">
+            <label>
+              <input
+                type="radio"
+                name="grid"
+                value="1"
+                :checked="gridMode === '1'"
+                @change="onGridChange"
+              />是
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="grid"
+                value="0"
+                :checked="gridMode === '0'"
+                @change="onGridChange"
+              />否
+            </label>
+          </div>
+        </div>
+        <div class="config_item">
+          <div class="label">主题模式：</div>
+          <div class="value radio_wrapper">
+            <label>
+              <input
+                type="radio"
+                name="theme"
+                value="light"
+                :checked="themeMode === 'light'"
+                @change="onThemeChange"
+              />
+              <span class="light_theme"></span>
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="theme"
+                value="dark"
+                @change="onThemeChange"
+                :checked="themeMode === 'dark'"
+              />
+              <span class="dark_theme"></span>
+            </label>
+          </div>
+        </div>
+        <div class="config_item"></div>
+        <!-- <div class="config_item">
+          <div class="label">页面比例：</div>
+          <select class="value select_item">
+            <option value="1440">3:4(1080:1440)</option>
+            <option value="1620">2:3(1080:1620)</option>
+            <option value="1500">自定义(1080:1500)</option>
+          </select>
+        </div> -->
+        <div class="config_item watermark">
+          <div class="label">关键字：</div>
+          <input class="value" placeholder="请输入关键字" v-model="watermark" />
+        </div>
       </div>
-      <div class="config_item">
-        <div class="label">每页行数：</div>
-        <input type="number" class="value" placeholder="请设置一页行数" v-model="pageSize" />
-      </div>
-      <div class="config_item">
-        <div class="label">最大页码：</div>
-        <input type="number" class="value" placeholder="请设置最大页码" v-model="maxPage" />
-      </div>
-      <div class="config_item watermark">
-        <div class="label">关键字：</div>
-        <input class="value" placeholder="请输入关键字" v-model="watermark" />
-      </div>
+      <button class="btn debugger_btn" @click="onDebugger">调试图片</button>
+      <button class="btn" @click="onDownload">下载图片</button>
     </div>
-    <button class="btn debugger_btn" @click="onDebugger">调试图片</button>
-    <button class="btn" @click="onDownload">下载图片</button>
+    <div class="preview">
+      <img :src="imgPreview" alt="" />
+    </div>
+    <div class="container" v-html="content"></div>
+    <div class="page_list" ref="pagesRef"></div>
   </div>
-  <div class="preview">
-    <img :src="imgPreview" alt="" />
-  </div>
-  <div class="container" v-html="content"></div>
-  <div class="pages" ref="pagesRef"></div>
 </template>
 
 <script setup>
@@ -36,17 +95,46 @@ import { ref, nextTick } from 'vue'
 import { dom } from './dom'
 import { addGridBg, addWaterMark, tagAToDownload } from './utils'
 
-const watermark = ref('')
-const firstPageSize = ref(6)
-const pageSize = ref(10)
-const maxPage = ref(50)
-const address = ref()
-// 'https://www.zhihu.com/market/paid_column/1636449898307194880/section/1637814430120218624'
+// 页面比例
+// const aspectRatio = 3 / 4
+// const height = 1080 / aspectRatio
+const height = 1500
+// 一行最大字数 34px:22 | 35px:21 | 36px:20
+const maxTextLength = 22
 
+// 是否打印网格背景(默认显示)
+const gridMode = ref('1')
+// 主题模式(默认浅色)
+const themeMode = ref('light')
+// 水印内容, 为空时不打印水印
+const watermark = ref('')
+// 首页显示行数
+const firstPageSize = ref(5)
+// 页面显示行数
+const pageSize = ref(8)
+// 截取的最大页码数
+const maxPage = ref(60)
+// 文章链接地址
+const address = ref()
+// const address = ref(
+//   'https://www.zhihu.com/market/paid_column/1638186406705827840/section/1641877398021672960'
+// )
+
+// 爬虫获取的知乎文章内容
 const content = ref(null)
+// 文章解析后要插入的 DOM 实例
 const pagesRef = ref(null)
+// 预览地址
 const imgPreview = ref('')
+// 是否调试模式
 const debuggerMode = ref(false)
+
+const onGridChange = (e) => {
+  gridMode.value = !!+e.target.value
+}
+const onThemeChange = (e) => {
+  themeMode.value = e.target.value
+}
 
 // 调试按钮
 const onDebugger = () => {
@@ -56,6 +144,7 @@ const onDebugger = () => {
     downloadImages()
   })
 }
+// 下载事件
 const onDownload = () => {
   if (!address.value || !address.value.trim()) {
     alert('请输入文章地址')
@@ -79,6 +168,7 @@ const onDownload = () => {
       })
     })
     .catch((err) => {
+      console.log(err)
       alert(err)
     })
 }
@@ -89,32 +179,41 @@ const downloadImages = async () => {
   pageList.forEach((p, i) => {
     setTimeout(() => {
       // 调试用代码
-      // i < 2 && getImage(p, i + 1)
+      // i < 1 && getImage(p, i + 1)
       getImage(p, i + 1)
-    }, i * 300)
+    }, i * 200)
   })
 }
 
 // 解析 pages DOM
 function insertPages() {
+  // 所有的 P 标签
   const pList = document.querySelectorAll('p')
+  // 组装一页的 P 标签
   let container = []
-  let pagenumber = 1
+  // 一页的 P 数量
+  let pCount = 0
+  // 页码
+  let pageNumber = 1
   pList.forEach((p, i) => {
     container.push(p)
-    // 第一页只截 firstPageSize 行 ||  || 最后一页
-    if (
-      i === firstPageSize.value - 1 ||
-      (i - firstPageSize.value) % pageSize.value === pageSize.value - 1 ||
-      i === pList.length - 1
-    ) {
+    const textLength = p.innerText.length
+
+    // 大概比例为
+    pCount = pCount + (Math.ceil(textLength / maxTextLength) + 1) * 0.5
+
+    // 第一页只截 firstPageSize 行 || 当页行数已满 || 最后一页
+    if (i === firstPageSize.value - 1 || pCount >= pageSize.value || i === pList.length - 1) {
+      // 组装页
       const div = document.createElement('div')
-      div.innerHTML = `<span class="number"> - ${pagenumber} -</span>`
+      div.innerHTML = `<span class="number"> - ${pageNumber} -</span>`
       div.setAttribute('class', 'page')
+      div.setAttribute('style', `height:${height}px`)
       pagesRef.value.appendChild(div)
       container.forEach((c) => div.appendChild(c))
       container = []
-      pagenumber++
+      pCount = 0
+      pageNumber++
     }
   })
   content.value = ''
@@ -123,15 +222,15 @@ function insertPages() {
 function getImage(targetEle, index) {
   return html2canvas(targetEle, {
     width: 1080,
-    height: 1440
+    height: height
     // backgroundColor: 'transparent'
   }).then((canvas) => {
     // 添加网格背景
-    addGridBg(canvas)
+    gridMode.value && addGridBg(canvas, themeMode.value)
 
     // 添加关键字水印
     const text = watermark.value.trim()
-    text && addWaterMark(canvas, text)
+    text && addWaterMark(canvas, text, themeMode.value)
 
     const base64Url = canvas.toDataURL('image/png')
     // jpeg 格式可以设置图片质量
--- src/utils.js
+++ src/utils.js
@@ -1,35 +1,44 @@
 // 平铺小网格背景
-export function addGridBg(canvas) {
+export function addGridBg(canvas, themeMode) {
   const ctx = canvas.getContext('2d')
   // 平铺网格
   let canvasGrid = document.createElement('canvas')
   canvasGrid.width = 60
   canvasGrid.height = 60
   const ctxGrid = canvasGrid.getContext('2d')
-  ctxGrid.strokeStyle = 'rgba(0, 0, 0, 0.1)'
+  if (themeMode === 'dark') {
+    ctxGrid.strokeStyle = 'rgba(255, 255, 255, 0.15)'
+  } else {
+    ctxGrid.strokeStyle = 'rgba(0, 0, 0, 0.1)'
+  }
   ctxGrid.strokeRect(0, 0, canvasGrid.width, canvasGrid.height)
   // 绘制网格
   ctx.fillStyle = ctx.createPattern(canvasGrid, 'repeat')
-  ctx.fillRect(0, 0, canvas.width * 10, canvas.height * 60)
+  ctx.fillRect(0, 0, canvas.width * 10, canvas.height * 100)
 }
 
 // 平铺水印
-export function addWaterMark(canvas, watermark) {
+export function addWaterMark(canvas, watermark, themeMode) {
   const ctx = canvas.getContext('2d')
 
   // 平铺水印
   let canvasWater = document.createElement('canvas')
   // 水印大小
-  canvasWater.width = canvas.width / 6
-  canvasWater.height = canvas.height / 10
+  canvasWater.width = canvas.width / 8
+  canvasWater.height = canvas.height / 20
 
   const ctxWater = canvasWater.getContext('2d')
   // ctxWater.strokeStyle = 'rgba(0, 0, 0, 0.1)'
   // ctxWater.strokeRect(0, 0, canvasWater.width, canvasWater.height)
   ctxWater.font = '16px Microsoft Yahei'
-  ctxWater.fillStyle = 'rgba(0, 0, 0, 0.2)'
+
+  if (themeMode === 'dark') {
+    ctxWater.fillStyle = 'rgba(255, 255, 255, 0.15)'
+  } else {
+    ctxWater.fillStyle = 'rgba(0, 0, 0, 0.07)'
+  }
   // 水平水印
-  ctxWater.fillText(watermark, 30, 60)
+  ctxWater.fillText(watermark, 0, 20)
   // 旋转水印
   // ctxWater.rotate((-20 * Math.PI) / 180)
   // ctxWater.fillText(watermark, 0, canvasWater.height / 2 + 20)
@@ -37,7 +46,7 @@ export function addWaterMark(canvas, watermark) {
 
   // 绘制重复的水印
   ctx.fillStyle = ctx.createPattern(canvasWater, 'repeat')
-  ctx.fillRect(0, 0, canvas.width * 10, canvas.height * 60)
+  ctx.fillRect(0, 0, canvas.width * 10, canvas.height * 100)
 }
 
 // 下载函数
